f7169581af20a61eafd8ea1fec98a954373db3eb
Dinesh <dineshadlus@gmail.com>
2021-02-04 20:26:03 -0600

    installing npm changes

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,21 @@
 #! /usr/bin/env groovy
 
 def call(String name = 'human') {
+
+
   echo "Hello, ${name}."
+
+  node ('testNode') {
+
+    stages {
+
+      stage('build'){
+
+        script {
+          sh 'npm install'
+        }
+        
+      }
+    }
+  }
 }
