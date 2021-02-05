7c4be6d41a34ad416d9b8c749207df9a97c339b1
Dinesh <dineshadlus@gmail.com>
2021-02-04 22:40:26 -0600

    installing npm changes-4

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -15,5 +15,23 @@ def call(String name = 'human') {
         }
       }
      }
+
+def call(Map config=[:], Closure body) {
+    node {
+        git url: "https://github.com/werne2j/sample-nodejs"
+        stage("Install") {
+            sh "npm install"
+        }
+        stage("Test") {
+            sh "npm test"
+        }
+        stage("Deploy") {
+            if (config.deploy) {
+                sh "npm publish"
+            }
+        }
+        body()
+    }
+}  
    
 }
