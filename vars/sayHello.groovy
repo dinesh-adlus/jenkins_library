30bf0b8d927696a3317de853aae30469e83e7db0
Dinesh <dineshadlus@gmail.com>
2021-02-11 13:21:37 -0600

    installing npm changes-10

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -26,7 +26,7 @@ def call(String name = 'sai') {
     //         bat "npm install"
     //     }
         stage("build") {
-            echo "$WORKSPACE"
+            bat 'build.sh'
         }
     }
 }  
