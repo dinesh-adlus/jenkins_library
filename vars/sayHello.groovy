54fbe76e9facc94c86f09350ebf1cad883ee142a
Dinesh Gurram <Dinesh.gurram@cvshealth.com>
2021-06-15 17:10:27 -0500

    modify

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,17 +3,25 @@
 import hudson.model.*
 
 def call(String name = 'sai') {
+
+
+
+pipeline {
+  agent any
+
+  tools {nodejs "nodejs"}
+
     node {
       stage("Checkout") {
          git url: "https://github.com/dinesh-adlus/angular-test-app"
       }
       echo "checkout is successfull"
-   
-      tools {nodejs "nodejs"}
+
       stage("Build"){
        sh (script: "sh build.sh", returnStdout: true)
       }  
              
     }
+   }
 }  
   
