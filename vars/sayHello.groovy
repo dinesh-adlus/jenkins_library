3e13df6bed5d19dbc4f2cfdeb6b2a34a6c798e47
Dinesh <dineshadlus@gmail.com>
2021-06-15 13:16:19 -0500

    etst

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -10,7 +10,7 @@ def call(String name = 'sai') {
       echo "checkout is successful"
         
       stage("Build"){
-         sh "$WORKSPACE/build.sh "
+         sh "ls"
       }  
              
     }
